629740d618e36335c2b91b471026662622fda571
Marcelo Mergulhao <marcelormergulhao@gmail.com>
2017-05-28 22:09:45 -0300

    Fix queries filters and string formatting

--- analyser.py
+++ analyser.py
@@ -20,7 +20,7 @@ def get_most_popular_articles():
     What are the most popular articles of all time?
     The method executes a database query and returns the result to the user"""
     query = """SELECT a.title,count(*) AS num_views FROM articles AS a, log AS l
-    WHERE l.path LIKE '%' || a.slug || '%' GROUP BY a.title
+    WHERE l.path = concat('/article/',a.slug) GROUP BY a.title
     ORDER BY num_views DESC LIMIT 3;"""
     return get_results_from_query(query)
 
@@ -31,7 +31,7 @@ def get_most_popular_author():
     The method executes a database query and returns the result to the user"""
     query = """SELECT au.name,cnt.num_views FROM authors AS au,
     (SELECT a.author,count(*) AS num_views FROM articles AS a,
-    log AS l WHERE l.path LIKE '%' || a.slug || '%' GROUP BY a.author
+    log AS l WHERE l.path = concat('/article/', a.slug) GROUP BY a.author
     ORDER BY num_views DESC) AS cnt WHERE au.id = cnt.author;"""
     return get_results_from_query(query)
 
@@ -53,18 +53,18 @@ if __name__ == "__main__":
     popular = get_most_popular_articles()
     print("\t\tA:The most viewed articles are:")
     for article in popular:
-        print("\t\t\t", article[0], "with", article[1], "views")
+        print("\t\t\t {} with {} views".format(article[0], article[1]))
 
     print("\t2-Who are the most popular article authors of all time?")
     authors = get_most_popular_author()
     print("\t\tA: The most popular authors are:")
     for author in authors:
-        print("\t\t\t", author[0], "with", author[1], "article views")
+        print("\t\t\t {} with {} article views".format(author[0], author[1]))
     print("\t3-On which days did more than 1% of requests lead to errors?")
     days = get_days_with_errors()
     print("\t\tA: On the following days:")
     for day in days:
         formated_day = day[0].strftime("%B %d, %Y")
-        formated_percentage = "{:.2f}".format(day[1]*100) + "%"
-        print("\t\t\t", formated_day, "with",
-              formated_percentage, "of them as errors")
+        formated_pct = "{:.2f}".format(day[1]*100) + "%"
+        print("\t\t\t {} with {} of them as errors".format(formated_day,
+                                                           formated_pct))
